8114137848d47e28dc706a341e332d176558c684
Isak Styf <isak.styf@masarinconsulting.se>
2019-10-25 21:07:42 +0200

    Added resolver code to actually return a fake temperature to the caller

--- resolver.go
+++ resolver.go
@@ -8,7 +8,9 @@ import (
 
 // THIS CODE IS A STARTING POINT ONLY. IT WILL NOT BE UPDATED WITH SCHEMA CHANGES.
 
-type Resolver struct{}
+type Resolver struct {
+	temperatures []*graphql1.Temperature
+}
 
 func (r *Resolver) Query() QueryResolver {
 	return &queryResolver{r}
@@ -17,5 +19,17 @@ func (r *Resolver) Query() QueryResolver {
 type queryResolver struct{ *Resolver }
 
 func (r *queryResolver) Temperatures(ctx context.Context) ([]*graphql1.Temperature, error) {
-	panic("not implemented")
+	if len(r.temperatures) == 0 {
+		temp := &graphql1.Temperature{
+			From: graphql1.Origin{
+				Device: "mydevice",
+				Lat:    12.3,
+				Lon:    16.8,
+			},
+			When: "1900-01-01T12:00:00",
+			Temp: 37.0,
+		}
+		r.temperatures = append(r.temperatures, temp)
+	}
+	return r.temperatures, nil
 }
